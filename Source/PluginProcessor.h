446b5970c05b989fa108c1f9f7a62ed02fd49db1
orp-com <oprojects777@gmail.com>
2024-09-19 11:48:37 -0400

    refactored dsp

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -136,6 +136,12 @@ private:
             }
         }
     }
+    
+    void updateLowCutFilters(const ChainSettings& chainSettings);
+    void updateHighCutFilters(const ChainSettings& chainSettings);
+    
+    void updateFilter();
+    
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessor)
 };
